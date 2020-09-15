1ccff546c2df54156e36256c5691a4042b43c3d1
Ankit kumar <ankitkhadria1@gmail.com>
2020-09-16 01:04:09 +0530

    added configuration for postgres

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,9 @@
 import os
+from environs import Env
+
+env = Env()
+# Read .env into os.environ
+env.read_env()
 
 # GENERAL
 # ------------------------------------------------------------------------------
@@ -79,12 +84,13 @@ TEMPLATES = [
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    "default": env.dj_db_url(
+        "DATABASE_URL", "postgres://postgres:password@localhost:5432/fake_django"
+    )
 }
+
 
 # PASSWORDS
 # ------------------------------------------------------------------------------
